fc6158e648137226a221cd1b125a02aa977cf638
Fernandavazgit1 <144569110+Fernandavazgit1@users.noreply.github.com>
2025-06-12 20:28:27 -0300

    Update dql.sql

--- game/dql.sql
+++ game/dql.sql
@@ -1,1 +1,6 @@
 
+-- Listar inventário de cada jogador
+SELECT i.id_inventario, p.nome AS jogador, i.pods
+FROM inventario i
+JOIN jogador j ON i.id_jogador = j.id_jogador
+JOIN personagem p ON j.id_personagem = p.id_personagem;
